50b790ee9af9db81600cd033ca8eec7b9e6a71bc
Fabian Schedler <dev.fabian.schedler@gmail.com>
2022-11-27 00:39:09 +0100

    KMM SPM package release for 0.1.6

--- Package.swift
+++ Package.swift
@@ -1,6 +1,8 @@
 // swift-tools-version:5.3
 import PackageDescription
 
+let remoteKotlinUrl = "https://api.github.com/repos/DatepollSystems/waiterrobot-mobile_android-shared/releases/assets/86076579.zip"
+let remoteKotlinChecksum = "a13448e12732580a975ef5e90504de57c96186aa9e1f97b7002c42e4a487d75e"
 let packageName = "shared"
 
 let package = Package(
@@ -17,7 +19,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: packageName,
-            path: "./shared/build/XCFrameworks/debug/\(packageName).xcframework"
+            url: remoteKotlinUrl,
+            checksum: remoteKotlinChecksum
         )
         ,
     ]
