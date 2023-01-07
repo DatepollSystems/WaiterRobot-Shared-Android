042402aae9de980b91bf95d52f2d7107f52d348b
runner <runner@Mac-1673103279705.local>
2023-01-07 15:14:11 +0000

    KMM SPM package release for 0.1.8

--- Package.swift
+++ Package.swift
@@ -1,6 +1,8 @@
 // swift-tools-version:5.3
 import PackageDescription
 
+let remoteKotlinUrl = "https://api.github.com/repos/DatepollSystems/waiterrobot-mobile_android-shared/releases/assets/90839047.zip"
+let remoteKotlinChecksum = "5275160cb0364b4a0193aee69f1be8f92e6ad34d063dac50c33ae60198713e92"
 let packageName = "shared"
 
 let package = Package(
@@ -17,7 +19,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: packageName,
-            path: "./shared/build/XCFrameworks/debug/\(packageName).xcframework"
+            url: remoteKotlinUrl,
+            checksum: remoteKotlinChecksum
         )
         ,
     ]
