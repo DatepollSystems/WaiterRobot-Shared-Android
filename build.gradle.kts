a3fce93fc1f0319c3185cb784da7f8aa1ee51c2c
Fabian Schedler <dev.fabian.schedler@gmail.com>
2023-02-10 21:06:59 +0100

    WR-202: Update gradle and agp

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
     }
     dependencies {
         val kotlinVersion = "1.8.10"
-        classpath("com.android.tools.build:gradle:7.3.1")
+        classpath("com.android.tools.build:gradle:7.4.1")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${kotlinVersion}")
         classpath("org.jetbrains.kotlin:kotlin-serialization:${kotlinVersion}")
     }
