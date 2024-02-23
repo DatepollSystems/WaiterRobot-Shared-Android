5ac81e42f2166b0d811c97ce5ef8821008232d58
Fabian Schedler <dev.fabian.schedler@gmail.com>
2024-02-24 00:41:10 +0100

    Migrate dialogs

--- androidApp/src/main/java/org/datepollsystems/waiterrobot/android/ui/core/ConfirmDialog.kt
+++ androidApp/src/main/java/org/datepollsystems/waiterrobot/android/ui/core/ConfirmDialog.kt
@@ -1,9 +1,9 @@
 package org.datepollsystems.waiterrobot.android.ui.core
 
-import androidx.compose.material.AlertDialog
-import androidx.compose.material.Button
-import androidx.compose.material.Text
-import androidx.compose.material.TextButton
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 
 @Composable
