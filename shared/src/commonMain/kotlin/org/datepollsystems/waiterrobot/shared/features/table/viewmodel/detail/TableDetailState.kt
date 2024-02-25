71efaef40b9a76a6f7a8ecb275d2f0b656794c52
Alexander Kauer <contact@kaulex.dev>
2024-02-25 12:56:13 +0100

    Added KotlinArray for TableDetails order items

--- shared/src/commonMain/kotlin/org/datepollsystems/waiterrobot/shared/features/table/viewmodel/detail/TableDetailState.kt
+++ shared/src/commonMain/kotlin/org/datepollsystems/waiterrobot/shared/features/table/viewmodel/detail/TableDetailState.kt
@@ -1,6 +1,7 @@
 package org.datepollsystems.waiterrobot.shared.features.table.viewmodel.detail
 
 import org.datepollsystems.waiterrobot.shared.core.data.Resource
+import org.datepollsystems.waiterrobot.shared.core.data.mapType
 import org.datepollsystems.waiterrobot.shared.core.viewmodel.ViewModelState
 import org.datepollsystems.waiterrobot.shared.core.viewmodel.ViewState
 import org.datepollsystems.waiterrobot.shared.features.table.models.OrderedItem
@@ -9,5 +10,10 @@ data class TableDetailState(
     override val viewState: ViewState = ViewState.Idle,
     val orderedItemsResource: Resource<List<OrderedItem>> = Resource.Loading()
 ) : ViewModelState() {
+
+    @Suppress("unused") // iOS only
+    val orderedItemsArray by lazy {
+        orderedItemsResource.mapType { it?.toTypedArray() }
+    }
     override fun withViewState(viewState: ViewState): TableDetailState = copy(viewState = viewState)
 }
