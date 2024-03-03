4a082ce7081d8137f209fccfe144cece930bd02c
Fabian Schedler <dev.fabian.schedler@gmail.com>
2024-03-03 13:40:18 +0100

    Use Badge for open order table indicator

--- androidApp/src/main/java/org/datepollsystems/waiterrobot/android/ui/tablelist/Table.kt
+++ androidApp/src/main/java/org/datepollsystems/waiterrobot/android/ui/tablelist/Table.kt
@@ -1,23 +1,16 @@
 package org.datepollsystems.waiterrobot.android.ui.tablelist
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Badge
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
 import org.datepollsystems.waiterrobot.android.ui.core.Preview
@@ -25,15 +18,11 @@ import org.datepollsystems.waiterrobot.shared.features.table.models.Table
 
 @Composable
 fun Table(table: Table, onClick: (Table) -> Unit) {
-    val cardShape = RoundedCornerShape(10)
     Card(
-        shape = cardShape,
-        modifier = Modifier
-            .aspectRatio(1f)
-            .clip(cardShape)
-            .clickable { onClick(table) }
+        onClick = { onClick(table) },
+        modifier = Modifier.aspectRatio(1f)
     ) {
-        BoxWithConstraints(
+        Box(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(10.dp),
@@ -44,17 +33,9 @@ fun Table(table: Table, onClick: (Table) -> Unit) {
                 style = MaterialTheme.typography.headlineMedium,
             )
             if (table.hasOrders) {
-                val size = maxWidth * 0.1f
-                Box(
-                    modifier = Modifier
-                        .size(size)
-                        .offset(
-                            x = -(maxWidth / 2 - size / 2),
-                            y = -(maxHeight / 2 - size / 2)
-                        )
-                        .clip(CircleShape)
-                        .background(MaterialTheme.colorScheme.onSurface)
-                )
+                Badge(
+                    modifier = Modifier.align(Alignment.TopEnd)
+                ) {} // content != null -> large Badge
             }
         }
     }
