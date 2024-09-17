1f911344c6ab8f368d2446688d2959685664f8fd
runner <runner@Mac-1726563735615.local>
2024-09-17 09:37:35 +0000

    KMM SPM package release for 1.6.8

--- Package.swift
+++ Package.swift
@@ -1,7 +1,11 @@
 // swift-tools-version:5.3
 import PackageDescription
 
+// BEGIN KMMBRIDGE VARIABLES BLOCK (do not edit)
+let remoteKotlinUrl = "https://maven.pkg.github.com/DatepollSystems/WaiterRobot-Shared-Android/org/datepollsystems/waiterrobot/shared-kmmbridge/1.6.8/shared-kmmbridge-1.6.8.zip"
+let remoteKotlinChecksum = "2717abea054d9e56c5c7c3115020a066ff3b8c7da184c538aab0925991959fb9"
 let packageName = "shared"
+// END KMMBRIDGE BLOCK
 
 let package = Package(
     name: packageName,
@@ -17,7 +21,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: packageName,
-            path: "./shared/build/XCFrameworks/debug/\(packageName).xcframework"
+            url: remoteKotlinUrl,
+            checksum: remoteKotlinChecksum
         )
         ,
     ]
